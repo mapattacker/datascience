fc092fa252495d47a2eb87d8ca027ad7ab4575b7
Jake Teo <siyang@SCMC-LT10s-MBP.local>
2017-07-01 20:11:22 +0800

    commit

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.datasets import load_iris
-
-# Load Iris data (https://en.wikipedia.org/wiki/Iris_flower_data_set)
-iris = load_iris()
-# Load iris into a dataframe and set the field names
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df.head()
-
